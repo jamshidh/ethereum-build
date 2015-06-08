4d1d814c2f4891b0322fca1cfaa8d33ad1f3fe56
jim <jim>
2015-06-08 08:39:18 -0700

    Cleanup, and modifying the upload script.  Also adding code to create the cpp client.

---
+++ GenesisInfo.cpp
@@ -0,0 +1,43 @@
+/*
+		This file is part of cpp-ethereum.
+
+		cpp-ethereum is free software: you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
+
+		cpp-ethereum is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/** @file GenesisInfo.cpp
+ * @author Gav Wood <i@gavwood.com>
+ * @date 2014
+ */
+
+#include "GenesisInfo.h"
+
+
+
+ 
+
+std::string const dev::eth::c_genesisInfo =
+R"ETHEREUM(
+{
+        "aced1ce9bb193d4270acf8738942ac7d008f22b4": { "wei", "1606938044258990275541962092341162602522202993782792835301376"}, // ace dice, 1b4ad 
+        "f1abb0d8af6f3de43cf05eb3d9458c95e79f30a0": { "wei", "1606938044258990275541962092341162602522202993782792835301376"}, // flab bod, 3aa58
+        "ba5e10071204f37931769d7afa454bc82e1eb4cd": { "wei", "1606938044258990275541962092341162602522202993782792835301376"}, // base 100, 3f30b
+        "a1e5acc2a0c6efa671d9e27c4faf60f22fc50de0": { "wei", "1606938044258990275541962092341162602522202993782792835301376"}, // ale sac, cb450
+        "bade1f4d04f56f13381b6e3dc0e78479fe2563c2": { "wei", "1606938044258990275541962092341162602522202993782792835301376"}, // bad elf, cb60c
+        "e1fd1e0689bc35a4b7e531f50f96d77b02149dbc": { "wei", "1606938044258990275541962092341162602522202993782792835301376"}, // elf die, e78bf
+        "dad1005e6487a99422b22a9db665e01148add52b": { "wei", "1606938044258990275541962092341162602522202993782792835301376"}, // dad loose, 78c2db
+        "fa15efeef38db9ff6f7eb862e3402c66ee0e7951": { "wei", "1606938044258990275541962092341162602522202993782792835301376"}, // false fee, 1e6392
+        "fadedcabf08aead902061c146c5458e5bea1ce9f": { "wei", "1606938044258990275541962092341162602522202993782792835301376"}, //faded cab, 2554ba
+        "5eedfab1e5e0ed81674dc6b503c6c89d413ccbc9": { "wei", "1606938044258990275541962092341162602522202993782792835301376"}, // seed fable, 675888
+         "1dea5c01dd17dee05b08c89e0753722f52f6d2f1": { "wei", "1606938044258990275541962092341162602522202993782792835301376"}, // idea scold, 7e1ada
+}
+)ETHEREUM";
